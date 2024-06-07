5a4158d76de5e7af7b99f15078b7f6ccd47fdfec
DuaIrfan2005 <155565372+DuaIrfan2005@users.noreply.github.com>
2024-06-08 00:06:46 +0500

    chapters 17 to 20

---
+++ app.js
@@ -0,0 +1,52 @@
+//question no 1//
+// var multiArray = [[]];
+
+//question no 4//
+// function generateTable() {
+   
+//    var tableNumber = document.getElementById('tableNumber').value;
+//    var tableLength = document.getElementById('tableLength').value;
+
+//    var result = "";
+
+//    if (tableNumber === "" || tableLength === "" || isNaN(tableNumber) || isNaN(tableLength)) {
+//        alert("Please enter valid numbers for both table number and length.");
+//        return;
+//    }
+
+//    tableNumber = parseInt(tableNumber);
+//    tableLength = parseInt(tableLength);
+
+//    for (var i = 1; i <= tableLength; i++) {
+//        result += tableNumber + " x " + i + " = " + (tableNumber * i) + "<br>";
+//    }
+
+//    document.getElementById('result').innerHTML = result;
+// }
+
+//question no 3//
+// var counting = "";
+
+// for (var i = 1; i <= 10; i++) {
+//     counting += i + "<br>";
+// }
+// document.getElementById('counting').innerHTML = counting;
+
+//question no 2//
+
+// var matrix = [
+//    [0, 1, 2, 3],
+//    [1, 0, 1, 2],
+//    [2, 1, 0, 1]
+// ];
+
+// var container = document.getElementById('matrix');
+
+// var matrixString = "";
+// for (var i = 0; i < matrix.length; i++) {
+//    for (var j = 0; j < matrix[i].length; j++) {
+//        matrixString += matrix[i][j] + " ";
+//    }
+//    matrixString += "<br>";
+// }
+// container.innerHTML = matrixString;
